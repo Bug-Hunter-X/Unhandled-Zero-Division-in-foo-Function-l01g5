611bae317c8d50852819f62a2c81f4932a389ce8
BugHunter-X <askzenai@gmail.com>
2025-02-18 17:26:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+function foo(a,b){    if(a === 0 || b === 0) {        return 0;    }    if (b === 0) {        return NaN; // Handle division by zero explicitly.    }    return a/b;}foo(0,0); //returns 0foo(1,0) //returns NaN
